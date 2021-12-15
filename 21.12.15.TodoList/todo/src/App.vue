517bd314067bcfafa717fe5d0d46fca938c668f3
Hoon-Hub <53948297+Hoon-Hub@users.noreply.github.com>
2021-12-15 18:02:47 +0900

    todoapp completed

--- 21.12.15.TodoList/todo/src/App.vue
+++ 21.12.15.TodoList/todo/src/App.vue
@@ -2,31 +2,41 @@
   <div id="app">
     <div class="container">
       <div class="col-md-6 offset-md-3">
-        <h1 class="text-center">Todo App</h1>
-        <input type="text" class="form-control" v-model="userInput" @keyup.enter="addNewTodo">
-        <button type="button" @click="addNewTodo">submit</button>
-      </div>
-      <div class="list-group text-left d-flex mb-4" >
-        <template  v-for="todo in activeTodoList" >
-          <button type="button" :key="todo.index" class="list-group-item text-left" @click="toggleTodoState(todo)">
-            {{ todo.label }}
-          </button>
-        </template>
-      </div>
-
-      <div class="text-right">
+        <div class="text-end">
         <button type="button" class="btn btn-sm" @click="changeCurrentState('active')">할일</button>
         <button type="button" class="btn btn-sm" @click="changeCurrentState('done')">완료</button>
         <button type="button" class="btn btn-sm" @click="changeCurrentState('all')">전체</button>
       </div>
+        <h1 class="text-center">Todo App</h1>
+        <div class="d-flex p-2  justify-content-between">
+          <input type="text" class="p-2 form-control" v-model="userInput" @keyup.enter="addNewTodo">
+          <button type="button" class="pl-3 btn btn-sm btn-primary" @click="addNewTodo">submit</button>
+        </div>
+
+      </div>
+      <div class="list-group text-start d-flex mb-4" >
+        <template  v-for="todo in activeTodoList" >
+          <todo
+            :label="todo.label"
+            :key="todo.index" 
+            @componentClick="toggleTodoState(todo)" 
+            >
+          </todo>
+        </template>
+      </div>
+
+      
     </div>
   </div>
 </template>
 
 <script>
-
+import Todo from '@/components/Todo'
 export default {
   name: 'App',
+  components:{
+    Todo
+  },
   data () {
     return {
       userInput: '',
@@ -60,5 +70,8 @@ export default {
 </script>
 
 <style>
-
+  .hover-event:hover {
+    background: #777777;
+    color: white;
+  }
 </style>
---
+++ 21.12.15.TodoList/todo/src/components/Todo.vue
@@ -0,0 +1,23 @@
+<template>
+  <button type="button" 
+            class="list-group-item text-left hover-event" 
+            @click="clickButton"
+          >
+            {{ label }}
+          </button>
+</template>
+
+<script>
+export default {
+    props: ['label'],
+    methods: {
+        clickButton () {
+            this.$emit('componentClick')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
