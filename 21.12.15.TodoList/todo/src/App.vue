2a3a58c20eda5fb7a2f025362d06da7f1c8611ae
Hoon-Hub <53948297+Hoon-Hub@users.noreply.github.com>
2021-12-15 16:18:20 +0900

    todoApp 기능 구현 완료.

--- 21.12.15.TodoList/todo/src/App.vue
+++ 21.12.15.TodoList/todo/src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="col-md-6 offset-md-3">
+        <h1 class="text-center">Todo App</h1>
+        <input type="text" class="form-control" v-model="userInput" @keyup.enter="addNewTodo">
+        <button type="button" @click="addNewTodo">submit</button>
+      </div>
+      <div class="list-group text-left d-flex mb-4" >
+        <template  v-for="todo in activeTodoList" >
+          <button type="button" :key="todo.index" class="list-group-item text-left" @click="toggleTodoState(todo)">
+            {{ todo.label }}
+          </button>
+        </template>
+      </div>
+
+      <div class="text-right">
+        <button type="button" class="btn btn-sm" @click="changeCurrentState('active')">할일</button>
+        <button type="button" class="btn btn-sm" @click="changeCurrentState('done')">완료</button>
+        <button type="button" class="btn btn-sm" @click="changeCurrentState('all')">전체</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      userInput: '',
+      todoList:[],
+      currentState: 'active'
+    }
+  },
+  computed: {
+    activeTodoList() {
+      return this.todoList.filter(todo => this.currentState === 'all' || todo.state === this.currentState)
+    }
+  },
+  methods: {
+    addNewTodo(){
+      if(this.userInput === '') return ''
+      this.todoList.push({
+        label: this.userInput,
+        state: 'active'
+      })
+      this.userInput = ''
+    },
+    toggleTodoState(todo) {
+      todo.state = todo.state === 'active' ? 'done' : 'active'
+    },
+    changeCurrentState(state){
+      this.currentState = state
+    }
+  },
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
