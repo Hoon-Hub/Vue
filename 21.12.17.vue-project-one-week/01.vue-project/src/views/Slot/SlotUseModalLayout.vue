a611ee49b68f99cda9c33f28155dad7368deb812
Hoon-Hub <53948297+Hoon-Hub@users.noreply.github.com>
2021-12-27 14:39:29 +0900

    slot

---
+++ 21.12.17.vue-project-one-week/01.vue-project/src/views/Slot/SlotUseModalLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <modal-layout>
+      <template v-slot:header>
+          <h1>팝업 타이틀</h1>
+      </template>
+      <template v-slot:default>
+          <p>pop up contents 1</p>
+          <p>pop up contents 2</p>
+      </template>
+      <template v-slot:footer>
+          <button type="button">닫기</button>
+      </template>
+  </modal-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
