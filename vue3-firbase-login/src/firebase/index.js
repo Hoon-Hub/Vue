9b5f4ef047bf46dcf5a8b4928873a50731697979
Hoon-Hub <gwi01304@naver.com>
2022-01-20 23:56:09 +0900

    vue-auth project ended

--- vue3-firbase-login/src/firebase/index.js
+++ vue3-firbase-login/src/firebase/index.js
@@ -1,17 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_cftTghb9afiRl2YwO4FywWp19GwFibY",
-  authDomain: "vue-fire-auth-d07f7.firebaseapp.com",
-  projectId: "vue-fire-auth-d07f7",
-  storageBucket: "vue-fire-auth-d07f7.appspot.com",
-  messagingSenderId: "657950541086",
-  appId: "1:657950541086:web:e99003b3a27c81c347f50f"
-};
+  apiKey: 'AIzaSyC_cftTghb9afiRl2YwO4FywWp19GwFibY',
+  authDomain: 'vue-fire-auth-d07f7.firebaseapp.com',
+  projectId: 'vue-fire-auth-d07f7',
+  storageBucket: 'vue-fire-auth-d07f7.appspot.com',
+  messagingSenderId: '657950541086',
+  appId: '1:657950541086:web:e99003b3a27c81c347f50f'
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
+
+const auth = getAuth(app)
+
+export { auth }
