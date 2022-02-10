2aec48d466d3a3e279a237c8eff91616e399fadc
Hoon-Hub <53948297+Hoon-Hub@users.noreply.github.com>
2022-02-10 17:13:01 +0900

    skeleton stucture created

---
+++ dropzone/src/views/DropZone.vue
@@ -0,0 +1,18 @@
+<template>
+ <div class="dropzone">
+   <span>Drag or Drop File</span>
+   <span>OR</span>
+   <label for="dropzoneFile">Select File</label>
+   <input type="file" id="dropzoneFile" />
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'DropZone',
+}
+</script>
+
+<style lang="scss">
+
+</style>
