f4b1ac6fd82588e67e4080fc056f409d57b1782c
Hoon-Hub <gwi01304@naver.com>
2022-02-10 23:26:58 +0900

    vue2 drag-drop

---
+++ dropzone-vue2/src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+   class="card"
+   :id="id"
+   :draggable="draggable"
+   @dragstart="dragStart"
+   @dragover.stop
+  >
+
+  </div>
+</template>
+
+<script>
+
+export default {
+    props: ['id', 'dragable'],
+    methods: {
+        dragStart: e => {
+            const target = e.target
+
+            e.dataTransfer.setData('card_id', target.id)
+
+            setTimeout(() => {
+                target.style.display = 'none'
+            }, 0)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
