a3c40b5e3b6b325b17a80825b30295df0db16654
Hoon-Hub <gwi01304@naver.com>
2022-02-15 00:34:14 +0900

    front, axios, tailwindcss

---
+++ location-project/client/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1 class="text-xl text-red-500">Testing Tailwind!</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
